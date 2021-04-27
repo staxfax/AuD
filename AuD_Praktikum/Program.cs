6f93e47b85334735e607d6c8b6e6e96944d0ab80
staxfax <a.f.stuerner@t-online.de>
2021-04-27 12:17:48 +0200

    Initial check-in of module AuD_Praktikum

---
+++ AuD_Praktikum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuD_Praktikum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
