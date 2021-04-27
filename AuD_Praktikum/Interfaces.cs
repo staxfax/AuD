aeaca58a50332d14ffd7fc9ec4fc8a971344ed06
staxfax <a.f.stuerner@t-online.de>
2021-04-27 17:00:15 +0200

    Interfaces + LinkedList

---
+++ AuD_Praktikum/Interfaces.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace AuD_Praktikum
+{
+    interface IDictionary
+    {
+        
+    }
+
+    interface ISetSorted : IDictionary
+    {
+
+    }
+
+    interface ISetUnsorted : IDictionary
+    {
+
+    }
+
+    interface IMultiSetSorted : IDictionary
+    {
+
+    }
+
+    interface IMultiSetUnsorted : IDictionary
+    {
+
+    }
+}
---
+++ AuD_Praktikum/LinkedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace AuD_Praktikum
+{
+    class SetSortedLinkedList : ISetSorted
+    {
+        
+    }
+
+    class SetUnsortedLinkedList : ISetUnsorted
+    {
+
+    }
+
+    class MultiSetSortedLinkedList : IMultiSetSorted
+    {
+
+    }
+
+    class MultiSetUnsortedLinkedList : IMultiSetUnsorted
+    {
+
+    }
+
+}
