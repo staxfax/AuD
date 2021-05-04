c293bab430f5921c26a82fc87c1e4cd83177937c
staxfax <a.f.stuerner@t-online.de>
2021-05-04 11:30:18 +0200

    update

--- AuD_Praktikum/Array.cs
+++ AuD_Praktikum/Array.cs
@@ -2,111 +2,111 @@
 
 namespace AuD_Praktikum
 {
-   
- //   abstract class Array
- //   {
- //       public int SIZE = 10;
- //       public int[] myArray;
 
- //       public Array()
- //       {
- //           myArray = new int[SIZE];
- //       }
+    //   abstract class Array
+    //   {
+    //       public int SIZE = 10;
+    //       public int[] myArray;
 
- //       public void print()
- //       {
- //           for (int i = 0; i < SIZE; i++)
- //           {
- //               Console.WriteLine(myArray(i));
- //           }
- //       }
- //   }
+    //       public Array()
+    //       {
+    //           myArray = new int[SIZE];
+    //       }
 
- //   class SetSortedArray : MultiSetSortedArray, ISetsorted
- //   {
-	
- //   }
+    //       public void print()
+    //       {
+    //           for (int i = 0; i < SIZE; i++)
+    //           {
+    //               Console.WriteLine(myArray(i));
+    //           }
+    //       }
+    //   }
 
-	//class SetUnsortedArray : MultiSetUnsortedArray, ISetUnsorted
- //   {
- //       public bool insert(int elem)
- //       {
+    //   class SetSortedArray : MultiSetSortedArray, ISetsorted
+    //   {
 
- //           if (search(elem))
- //               return false;
+    //   }
 
- //           else
- //              return base.insert(elem);
- //       }
+    //class SetUnsortedArray : MultiSetUnsortedArray, ISetUnsorted
+    //   {
+    //       public bool insert(int elem)
+    //       {
 
+    //           if (search(elem))
+    //               return false;
 
- //   }
-
-	//class MultiSetSortedArray : Array, IMultiSetSorted
- //   {
-
- //   }
-
-	//class MultiSetUnsortedArray : Array, IMultiSetUnsorted
- //   {
- //       public bool insert(int elem)
- //       {
- //           for (int i = 0; i < SIZE; i++)
- //           {
- //               if (myArray[i] == null)
- //               {
- //                   myArray[i] = elem;
- //                   return true;
- //               } 
- //           }
-
- //           return false;
- //       }
-
- //       public bool search(int elem)
- //       {
-
- //           int i = _search_(elem);
-
- //           if (i > -1)
- //               return true;
- //           else
- //               return false;
- //       }
-
- //       private int _search_(int elem)
- //       {
- //           int i = 0;
-
- //           while (myArray[i] != elem)
- //           {
- //               i++;
-
- //               if (i == SIZE)
- //                   return -1;
- //           }
-
- //           return i;
- //       }
-
- //       public bool delete(int elem)
- //       {
- //           int i = _search_(elem);
-
- //           if (i == -1)
- //               return false;
+    //           else
+    //              return base.insert(elem);
+    //       }
 
 
- //           for (int j = 0; j < SIZE; j++)
- //           {
- //               if (myArray[j] == null)
- //               {
- //                   myArray[i] = myArray[j--];
- //                   return true;
- //               }
- //           }
+    //   }
 
- //           return false;
- //       }
-    }
+    //class MultiSetSortedArray : Array, IMultiSetSorted
+    //   {
+
+    //   }
+
+    //class MultiSetUnsortedArray : Array, IMultiSetUnsorted
+    //   {
+    //       public bool insert(int elem)
+    //       {
+    //           for (int i = 0; i < SIZE; i++)
+    //           {
+    //               if (myArray[i] == null)
+    //               {
+    //                   myArray[i] = elem;
+    //                   return true;
+    //               } 
+    //           }
+
+    //           return false;
+    //       }
+
+    //       public bool search(int elem)
+    //       {
+
+    //           int i = _search_(elem);
+
+    //           if (i > -1)
+    //               return true;
+    //           else
+    //               return false;
+    //       }
+
+    //       private int _search_(int elem)
+    //       {
+    //           int i = 0;
+
+    //           while (myArray[i] != elem)
+    //           {
+    //               i++;
+
+    //               if (i == SIZE)
+    //                   return -1;
+    //           }
+
+    //           return i;
+    //       }
+
+    //       public bool delete(int elem)
+    //       {
+    //           int i = _search_(elem);
+
+    //           if (i == -1)
+    //               return false;
+
+
+    //           for (int j = 0; j < SIZE; j++)
+    //           {
+    //               if (myArray[j] == null)
+    //               {
+    //                   myArray[i] = myArray[j--];
+    //                   return true;
+    //               }
+    //           }
+
+    //           return false;
+    //       }
+    //  }
 }
--- AuD_Praktikum/LinkedList.cs
+++ AuD_Praktikum/LinkedList.cs
@@ -3,76 +3,95 @@ namespace AuD_Praktikum
 {
     //// Insgesamt vier Klassen für verkettete Listen
 
-    //abstract class LElem : IDictionary
+    abstract class LinkedList : IDictionary
+    {
+        private class LElem
+        {
+            public int elem;
+           
+            public LElem next, prev; // einfach oder doppelt verkette Liste besser??
+
+            public LElem(int elem) { this.elem = elem; }
+        }
+
+        LElem first, last;
+
+        // print() bei einfach und doppelt verkettet ohnehin gleich
+        public void print()
+        {
+            for (LElem tmp = first; tmp != null; tmp = tmp.next)
+                Console.WriteLine(tmp.elem);
+        }
+
+        // Suche, noch zu ergänzen: Zwicshenspeicher der Position des gefundenen Elements
+        public bool search(int elem)
+        {
+            // Fall 1: Liste ist leer
+            // Fall 2: Das gesuchte Element befindet sich am Anfang
+            // Fall 3: Das gesuchte Element befindet sich am Ende
+            // Fall 4: Das gesuchte Element befindet sich mittendrin
+
+            if (first == null) // Fall 1
+                return false;
+            if (first.elem.CompareTo(elem) == 0) // Fall 2
+                return true;
+            else if (last.elem.CompareTo(elem) == 0) // Fall 3
+                return true;
+            else
+            {
+                LElem tmp = first.next;
+                while (tmp.next != null && tmp.elem.CompareTo(elem) != 0)
+                    tmp = tmp.next;
+                if (tmp.next != null)
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+        // Löschen
+        public bool delete(int elem)
+        {
+            search(elem);
+            return false;
+        }
+
+        // Einfügen, wird Listen spezifisch implementiert, da nicht bei allen gleich
+        public abstract bool insert(int elem);
+
+
+    }
+
+    //class SetSortedLinkedList : LElem
     //{
-    //    protected string elem;
-    //    protected LElem next, prev; // einfach oder doppelt verkette Liste??
-
-    //    protected LElem(string elem) { this.elem = elem; }
-
-    //    LElem first, last;
-
-    //    // print() bei einfach und doppelt verkettet ohnehin gleich
-    //    void IDictionary.print()
+    //    public override bool insert(int elem)
     //    {
-    //        for (LElem tmp = first; tmp != null; tmp = tmp.next)
-    //            Console.WriteLine(tmp.elem);
+    //        return false;
     //    }
+    //}
 
-    //    bool IDictionary.search(int elem)
+    //class SetUnsortedLinkedList : LElem
+    //{
+    //    public override bool insert(int elem)
     //    {
-    //        // Fall 1: Liste ist leer
-    //        // Fall 2: Das gesuchte Element befindet sich am Anfang
-    //        // Fall 3: Das gesuchte Element befindet sich am Ende
-    //        // Fall 4: Das gesuchte Element befindet sich mittendrin
-
-    //        if (first == null) // Fall 1
-    //            return false;
-    //        if (first.elem.CompareTo(elem) == 0) // Fall 2
-    //            return true;
-    //        else if (last.elem.CompareTo(elem) == 0) // Fall 3
-    //            return true;
-    //        else
-    //        {
-    //            LElem tmp = first.next;
-    //            while (tmp.next != null && tmp.elem.CompareTo(elem) != 0)
-    //                tmp = tmp.next;
-    //            if (tmp.next != null)
-    //                return true;
-    //            else
-    //                return false;
-    //        }
+    //        return false;
     //    }
-
-    //    //bool IDictionary.delete(int elem)
-    //    //{
-    //    //    IDictionary.search(elem);
-    //    //}
-
-    //    //abstract bool IDictionary.insert(int elem) { }
-
-
     //}
 
-
-    //class SetSortedLinkedList : ISetSorted
+    //class MultiSetSortedLinkedList : LElem
     //{
-
+    //    public override bool insert(int elem)
+    //    {
+    //        return false;
+    //    }
     //}
 
-    //class SetUnsortedLinkedList : ISetUnsorted
+    //class MultiSetUnsortedLinkedList : LElem
     //{
-
-    //}
-
-    //class MultiSetSortedLinkedList : IMultiSetSorted
-    //{
-
-    //}
-
-    //class MultiSetUnsortedLinkedList : IMultiSetUnsorted
-    //{
-
+    //    public override bool insert(int elem)
+    //    {
+    //        return false;
+    //    }
     //}
 
 }
