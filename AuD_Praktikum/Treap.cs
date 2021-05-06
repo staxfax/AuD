4dc3769c89a6aaaabd0c4bdba1f57e9217712943
Olesya Stetsenko <stetsenkool78477@th-nuernberg.de>
2021-05-06 15:41:10 +0200

    Initalize Treap.cs

---
+++ AuD_Praktikum/Treap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuD_Praktikum
+{
+    class TreapNode : BinTreeNode
+    {
+        public int priority;
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}; {priority,3}";
+        }
+
+    }
+    class Treap : BinSearchTree
+    {
+        private TreapNode root;
+
+        public Treap()
+        {
+            root = null;
+        }
+
+    }
+}
