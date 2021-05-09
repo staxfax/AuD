48cdb502f55992e5b0d2a556acc5847bc09f50f5
Olesya Stetsenko <stetsenkool78477@th-nuernberg.de>
2021-05-09 14:25:47 +0200

    Treap.cs neu, RotationHeap, DownHeap, BuildHeap initialisiert

--- AuD_Praktikum/Treap.cs
+++ AuD_Praktikum/Treap.cs
@@ -8,6 +8,11 @@ namespace AuD_Praktikum
     {
         public int priority;
 
+        public TreapNode(int priority)
+        {
+            this.priority = priority;
+        }
+
         public override string ToString()
         {
             return $"{base.ToString()}; {priority,3}";
@@ -16,17 +21,73 @@ namespace AuD_Praktikum
     }
     class Treap : BinSearchTree
     {
+        private Random random;
+
         public Treap()
         {
             root = null;
+            random = new Random();
         }
 
-        public TreapNode search(int elem)
+        protected override BinTreeNode insertNode(int elem)
         {
-            return null;// searchNode();
+            TreapNode a = new TreapNode(random.Next(0, 51));
+            a.zahl = elem;
+            a.left = null; a.right = null;
+            return a;
         }
 
-       
+        public void RotationHeap(TreapNode n)
+        {
+            bool left = n.left.zahl < n.right.zahl;
+            if (left)
+            {
+                if (n.zahl > n.left.zahl)
+                {
+                    BinTreeNode braun = n.left;
+                    n.left = braun.left;
+                    //tausche die Verbindungen nach rechts
+                    BinTreeNode blau = n.right;
+                    n.right = braun.right;
+                    braun.right = blau;
+                    //Vorgänger zeigt auf Nachfolger
+                    pred.left = braun;
+                    braun.left = n;
+                }
+            }
+            else
+            {
+                if (n.zahl > n.right.zahl)
+                {
+                    BinTreeNode braun = n.right;
+                    n.right = braun.right;
+                    //tausche die Verbindungen nach links
+                    BinTreeNode blau = n.left;
+                    n.left = braun.left;
+                    braun.left = blau;
+                    //Vorgänger zeigt auf Nachfolger
+                    pred.right = braun;
+                    braun.right = n;
+                }
+            }
+        }
+
+        public void DownHeap(TreapNode n)
+        {
+            if(n != null)
+            {
+                BinTreeNode found = searchNode(n.zahl);
+                if(found != null)
+                {
+                    RotationHeap(n);
+                }
+            }
+        }
+
+        public void BuildHeap()
+        {
+
+        }
     }
 
 
