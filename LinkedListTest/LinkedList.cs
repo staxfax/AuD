c7291b94aedfb9464f4486f87a023618418a7a1b
staxfax <a.f.stuerner@t-online.de>
2021-06-15 10:17:15 +0200

    final version (hoffentlich)

--- LinkedListTest/LinkedList.cs
+++ LinkedListTest/LinkedList.cs
@@ -8,11 +8,14 @@ namespace AuD_Praktikum
             public int elem;
 
             public LElem next = null;
+            //public LElem prev = null;
 
             public LElem(int elem) { this.elem = elem; }
         }
 
-        protected LElem first = null, last = null, position = null, prevposition = null;
+        protected LElem first = null, last = null, position = null;
+
+        //public int? index; // Index zur Zwischenspeicherung der Position nach Suche
 
         protected int count; // Anzahl der Elemente in der Liste
 
@@ -25,11 +28,12 @@ namespace AuD_Praktikum
         }
 
         // Suche
-        public bool search(int elem) // Speichert das jeweils vorderste gefundene Element zwischen
+        public bool search(int elem)
         {
             position = null;
             if (first == null) // Liste ist leer
             {
+                //position = null;
                 return false;
             }
             else if (first.elem.CompareTo(elem) == 0) // Das gesuchte Element befindet sich am Anfang
@@ -40,19 +44,17 @@ namespace AuD_Praktikum
             else // Das gesuchte Element befindet sich mittendrin ODER am Ende
             {
                 LElem tmp = first;
-                while (tmp.elem.CompareTo(elem) != 0) // 0: tmp.elem = elem -> Suche gibt Position des jeweils vordersten elem zurück, vor dem dann eingefügt wird
+                while (tmp.elem.CompareTo(elem) != 0) // 0: tmp.elem = elem -> Suche gibt Position des jeweils HINTERSTEN elem zurück, nach dem dann eingefügt wird
                 {
-                    if (tmp.next != null) // Position vor dem gesuchten Element wird zwischengespeichert (nötig für delete)
-                        if (tmp.next.elem.CompareTo(elem) == 0)
-                            prevposition = tmp;
-                    if (tmp.elem.CompareTo(elem) <= 0) // Falls elem nicht vorhanden, wird zusätzlich Stelle des größten kleineren elem gespeichert (nötig für Sorted insert)
+                    if (tmp.elem.CompareTo(elem) < 1) // Falls elem nicht vorhanden, wird zusätzlich Stelle des größten kleineren elem gespeichert (nötig für Sorted insert)
                         position = tmp;
                     if (tmp.next == null)
                         break;
                     tmp = tmp.next;
                 }
-                if (tmp.next != null) // While stoppte obwohl Liste nicht zu Ende, d.h. Element gefunden
+                if (tmp.next != null) // While stoppte obwohl Liste nicht zu Ende -> true
                 {
+                    //position = tmp;
                     return true;
                 }
                 else if (last.elem.CompareTo(elem) == 0) // Das gesuchte Element befindet sich am Ende
@@ -68,7 +70,8 @@ namespace AuD_Praktikum
         }
 
         // Löschen
-        public bool delete(int elem) // Löscht das jeweils vorderste gefundene Element
+        // löscht das jeweils 'vorderste' gefundene Element
+        public bool delete(int elem)
         {
             if (search(elem) == true)
             {
@@ -80,25 +83,36 @@ namespace AuD_Praktikum
                 }
                 else // Mindestens zwei Elemente
                 {
-                    if (first.elem.CompareTo(elem) == 0) // Erstes Element löschen
+                    if (position == first) // Erstes Element löschen
                     {
                         first = first.next;
+                        //first.prev = null;
                         count--;
                         return true;
                     }
-                    else if (prevposition.next != last) // Element mittendrin löschen
+                    else if (position != last) // Element mittendrin löschen
                     {
-                        prevposition.next = prevposition.next.next;
+                        //position.prev.next = position.next;
+                        //position.next.prev = position.prev;
+                        
+                        position.next.next = position.next.next.next;
                         count--;
                         return true;
                     }
                     else // Letztes Element löschen
                     {
-                        prevposition.next = null;
-                        last = prevposition;
+                        //last = last.prev;
+
+                        LElem tmp = first;
+                        while (tmp.next != position)
+                            tmp = tmp.next;
+                        tmp.next = tmp.next.next;
+                        position.next = null;
+                        last = position;
                         count--;
                         return true;
                     }
+
                 }
             }
             else { return false; }
@@ -118,14 +132,14 @@ namespace AuD_Praktikum
         {
             LElem nelem = new LElem(elem);
             search(elem);
-
+            //142175
             if (first == null) // Liste leer
             {
                 first = last = nelem;
                 count++;
                 return true;
             }
-            else if (first == last) // Liste hat nur ein Element
+            else if (count == 1) // Liste hat nur ein Element
             {
                 if (elem > last.elem) // Add End
                 {
@@ -142,23 +156,27 @@ namespace AuD_Praktikum
                     return true;
                 }
             }
-            else if (elem < first.elem) // Fügt vor aktuellem first einfügen
-            {
-                nelem.next = first;
-                first = nelem;
-                count++;
-                return true;
-            }
+            //else if (position == first) // Am Anfang einfügen
+            //{
+            //    nelem.next = first;
+            //    //first.prev = nelem;
+            //    first = nelem;
+            //    count++;
+            //    return true;
+            //}
             else if (position != last) // Mittendrin einfügen - Add After position
             {
-                nelem.next = position.next;
-                position.next = nelem;
+                nelem.next = position.next; // Vorwärtsverkettung vom neuen Element zur Restliste
+                //nelem.prev = position;      // Rückwärtsverkettung vom neuen Elenent zum Listenanfangsstück
+                //position.next.prev = nelem; // Rückwärtsverkettung vom nachfolgenden Listenelement
+                position.next = nelem;      // Vorwärtsverkettung zum neuen Element
                 count++;
                 return true;
             }
             else // Am Ende einfügen
             {
                 last.next = nelem;
+                //nelem.prev = last;
                 last = last.next;
                 count++;
                 return true;
@@ -180,6 +198,7 @@ namespace AuD_Praktikum
             else // Liste nicht leer -> AddEnd
             {
                 last.next = nelem;
+                //nelem.prev = last;
                 last = last.next;
                 count++;
                 return true;
@@ -192,12 +211,12 @@ namespace AuD_Praktikum
     {
         public override bool insert(int elem)
         {
-            if (search(elem) == false) // Element noch nicht vorhanden
+            if (search(elem) == false)
             {
                 base.insert(elem);
                 return true;
             }
-            else // Element bereits vorhanden
+            else
                 return false;
         }
     }
@@ -215,4 +234,31 @@ namespace AuD_Praktikum
                 return false;
         }
     }
+
+    class TestRun
+    {
+        public static void Main(string[] args)
+        {
+            MultiSetSortedLinkedList l1 = new MultiSetSortedLinkedList();
+
+            l1.insert(1);
+            l1.insert(2);
+            l1.insert(4);
+            l1.insert(2);
+            l1.insert(1);
+            l1.insert(2);
+            l1.insert(7);
+            l1.insert(5);
+            l1.insert(9);
+
+            l1.delete(1);
+            l1.delete(5);
+            l1.delete(9);
+            l1.delete(2);
+
+            l1.print();
+
+        }
+    }
+
 }
